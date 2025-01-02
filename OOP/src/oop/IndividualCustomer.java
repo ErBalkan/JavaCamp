f4726f7f796f8347f9b82cc4d61a42383d99140c
erbalkan <errbalkan@gmail.com>
2025-01-03 00:22:23 +0300

    2. Ders işlendi.

---
+++ OOP/src/oop/IndividualCustomer.java
@@ -0,0 +1,23 @@
+package oop;
+
+public class IndividualCustomer extends Customer {
+	private String firstName;
+	private String lastName;
+
+	public String getFirstNmae() {
+		return firstName;
+	}
+
+	public void setFirstNmae(String firstNmae) {
+		this.firstName = firstNmae;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
